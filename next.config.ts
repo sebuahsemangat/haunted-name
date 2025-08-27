2f30f8576f6076a2033299c392d658e93cd49930
Apep Wahyudin <mrsyntax25@gmail.com>
2025-08-27 03:13:18 +0000

    gambar tetap tidak tampil

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'aifirebase-7a250.appspot.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
